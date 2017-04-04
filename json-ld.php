180cfa28e696ed1e180a79276bc62b2ae5956cc4
John Niumata <johnny@niumata.com>
2017-04-04 11:00:35 -0400

    Added drop down of local business types

--- json-ld.php
+++ json-ld.php
@@ -57,6 +57,165 @@
 	</div>
 	<h2>Json-ld generator tool</h2>
 	<form method="get" action="process-ld.php">
+		Choose one: <select name="orgType" data-path="@type">
+                <option>Local Business</option>
+		<option>Animal Shelter</option>
+                <optgroup label='Automotive' id='automotive'>
+                  Automotive <option>Automotive Business</option>
+                  <option>Auto Body Shop</option>
+                  <option>Auto Dealer</option>
+                  <option>Auto Parts Store</option>
+                  <option>Auto Rental</option>
+                  <option>Auto Repair</option>
+                  <option>Auto Wash</option>
+                  <option>Gas Station</option>
+                  <option>Motorcycle Dealer</option>
+                  <option>Motorcycle Repair</option>
+                </optgroup>
+                <option>Child Care</option>
+                <option>Dry Cleaning Or Laundry</option>
+                <optgroup label='Emergency'>
+                  <option>Emergency Service</option>
+                  <option>Fire Station</option>
+                  <option>Hospital</option>
+                  <option>Police Station</option>
+                </optgroup>
+                <option>Employment Agency</option>
+                <optgroup label="Entertainment">
+                  <option>Entertainment Business</option>
+                  <option>Adult Entertainment</option>
+                  <option>Amusement Park</option>
+                  <option>Art Gallery</option>
+                  <option>Casino</option>
+                  <option>Comedy Club</option>
+                  <option>Movie Theater</option>
+                  <option>Night Club</option>
+                </optgroup>
+                <optgroup label='Financial'>
+                  <option>Accounting Service</option>
+                  <option>Automated Teller</option>
+                  <option>Bank Or Credit Union</option>
+                  <option>Insurance Agency</option>
+                </optgroup>
+                <optgroup label='Food'>
+                  <option>Food Establishment</option>
+                  <option>Bakery</option>
+                  <option>Bar Or Pub</option>
+                  <option>Brewery</option>
+                  <option>Cafe Or Coffee Shop</option>
+                  <option>Fast Food Restaurant</option>
+                  <option>Ice Cream Shop</option>
+                  <option>Restaurant</option>
+                  <option>Winery</option>
+                </optgroup>
+                <option>Government Office</option>
+                <option>Post Office</option>
+                <optgroup label='Health And Beauty'>
+                  <option>Health And Beauty Business</option>
+                  <option>Beauty Salon</option>
+                  <option>Day Spa</option>
+                  <option>Hair Salon</option>
+                  <option>Health Club</option>
+                  <option>Nail Salon</option>
+                  <option>Tattoo Parlor</option>
+                </optgroup>
+                <optgroup label='Home And Construction'>
+                  <option>Home And Construction Business</option>
+                  <option>Electrician</option>
+                  <option>General Contractor</option>
+                  <option>HVAC Business</option>
+                  <option>House Painter</option>
+                  <option>Locksmith</option>
+                  <option>Moving Company</option>
+                  <option>Plumber</option>
+                  <option>RoofingContractor</option>
+                </optgroup>
+                <option>Internet Cafe</option>
+                <option>Library</option>
+                <optgroup label='Lodging'>
+                  <option>Lodging Business</option>
+                  <option>Bed And Breakfast</option>
+                  <option>Hostel</option>
+                  <option>Hotel</option>
+                  <option>Motel</option>
+                </optgroup>
+                <optgroup label='Medical/Dental'>
+                  <option>Dentist</option>
+                  <option>Diagnostic Lab</option>
+                  <option>Hospital</option>
+                  <option>Medical Clinic</option>
+                  <option>Optician</option>
+                  <option>Pharmacy</option>
+                  <option>Physician</option>
+                  <option>Veterinary Care</option>
+                </optgroup>
+                <optgroup label='Professional Services'>
+                  <option>Professional Service</option>
+                  <option>Accounting Service</option>
+                  <option>Attorney</option>
+                  <option>Dentist</option>
+                  <option>Electrician</option>
+                  <option>General Contractor</option>
+                  <option>House Painter</option>
+                  <option>Locksmith</option>
+                  <option>Notary</option>
+                  <option>Plumber</option>
+                  <option>Roofing Contractor</option>
+                </optgroup>
+                <option>Radio Station</option>
+                <option>Real Estate Agent</option>
+                <option>Recycling Center</option>
+                <option>Self Storage</option>
+                <option>Shopping Center</option>
+                <optgroup label='Sports Activities'>
+                  <option>Sports Activity Location</option>
+                  <option>Bowling Alley</option>
+                  <option>Exercise Gym</option>
+                  <option>Golf Course</option>
+                  <option>Health Club</option>
+                  <option>Public Swimming Pool</option>
+                  <option>Ski Resort</option>
+                  <option>Sports Club</option>
+                  <option>Stadium Or Arena</option>
+                  <option>Tennis Complex</option>
+                </optgroup>
+                <optgroup label='Stores'>
+                  <option>Store</option>
+                  <option>Auto Parts Store</option>
+                  <option>Bike Store</option>
+                  <option>Book Store</option>
+                  <option>Clothing Store</option>
+                  <option>Computer Store</option>
+                  <option>Convenience Store</option>
+                  <option>Department Store</option>
+                  <option>Electronics Store</option>
+                  <option>Florist</option>
+                  <option>Furniture Store</option>
+                  <option>Garden Store</option>
+                  <option>Grocery Store</option>
+                  <option>Hardware Store</option>
+                  <option>Hobby Shop</option>
+                  <option>Home Goods Store</option>
+                  <option>Jewelry Store</option>
+                  <option>Liquor Store</option>
+                  <option>Mens Clothing Store</option>
+                  <option>Mobile Phone Store</option>
+                  <option>Movie Rental Store</option>
+                  <option>Music Store</option>
+                  <option>Office Equipment Store</option>
+                  <option>Outlet Store</option>
+                  <option>Pawn Shop</option>
+                  <option>Pet Store</option>
+                  <option>Shoe Store</option>
+                  <option>Sporting Goods Store</option>
+                  <option>Tire Shop</option>
+                  <option>Toy Store</option>
+                  <option>Wholesale Store</option>
+                </optgroup>
+                <option>Television Station</option>
+                <option>Tourist Information Center</option>
+                <option>Travel Agency</option>
+              </select><br>
 		<input type="text" id="name" name="name" placeholder="Business name">
 		<input type="text" id="address" name="address" placeholder="address">
 		<input type="text" id="city" name="city" placeholder="city">
@@ -72,7 +231,7 @@
 		<input type="text" id="priceRange" name="priceRange" placeholder="price range e.g. $40 - 60">
 
 		<textarea id="description" name="description" placeholder="description"></textarea>
-		<input type="text" id="additional" name="additional" placeholder="additional type e.g. http://www.productontology.org/id/Flooring" value="http://www.productontology.org/id/Flooring">
+		
 		<input type="text" id="map" name="map" placeholder="map url">
 		<span class="coord" style="display:none;padding:3px;border:1px solid #cccccc;background-color:#eee;cursor: pointer; margin: 0 auto; width: 30%;">fill lat & long co-ordinates below</span>
 		<input type="text" id="lat" name="lat" placeholder="lat">
